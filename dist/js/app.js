3b2ca0e3e86c09f011c5a8f43af53af758554757
Aleksandr <shaban4@yandex.ru>
2024-05-13 20:22:09 +0700

    fix bug 13.05

--- dist/js/app.js
+++ dist/js/app.js
@@ -10783,7 +10783,6 @@ PERFORMANCE OF THIS SOFTWARE.
             notequalto: "Это значение должно отличаться."
         });
         document.addEventListener("click", (function(e) {
-            sidebarCatalogActions(e);
             if (e.target.closest(".form__clear-svg")) {
                 let input = e.target.closest(".form__line").querySelector(".form__input") || e.target.closest(".form__line").querySelector(".form__txt");
                 input.value = "";
@@ -10792,27 +10791,7 @@ PERFORMANCE OF THIS SOFTWARE.
                 e.target.closest(".form__clear-svg").classList.remove("_active");
             }
             if (e.target.closest("textarea")) txtarAutoHeight(e.target);
-            if (e.target.closest(".personal-data__change")) {
-                changeData(e.target);
-                e.preventDefault();
-            }
-            if (e.target.closest(".order__more-btn")) {
-                let target = e.target.closest(".order__more-btn");
-                target.classList.contains("_spoller-active") ? target.innerHTML = "Свернуть детали заказа" : target.innerHTML = "Показать детали заказа";
-                e.preventDefault();
-            }
         }));
-        const filtersPopup = document.querySelector("#filters-more");
-        if (filtersPopup) {
-            filtersPopup.remove();
-            document.querySelector(".popup-box").insertAdjacentElement("beforeend", filtersPopup);
-            getFilterColumns(filtersPopup);
-        }
-        function getFilterColumns(popup) {
-            const columns = popup.querySelectorAll(".filters__col");
-            const popupWrapper = popup.querySelector(".filters__wrapper");
-            columns.length > 1 ? popupWrapper.classList.add("many-cols") : null;
-        }
         let shareButton = document.getElementById("share-button");
         if (shareButton) {
             let thisUrl = window.location.href;
@@ -10864,177 +10843,6 @@ PERFORMANCE OF THIS SOFTWARE.
                 }));
             }
         }
-        function changeData(target) {
-            let el = target.closest(".personal-data__row");
-            el.classList.add("_active");
-            let submitBtn = el.querySelector(".personal-data__btn");
-            submitBtn.addEventListener("click", (function(e) {
-                el.classList.remove("_active");
-                el.classList.add("show-msg");
-                setTimeout((() => {
-                    el.classList.remove("show-msg");
-                }), 3e3);
-            }));
-            document.addEventListener("keydown", (function(e) {
-                if (e.code === "Escape" || e.code === "Enter" || e.code === "NumpadEnter") {
-                    el.classList.remove("_active");
-                    el.classList.add("show-msg");
-                    setTimeout((() => {
-                        el.classList.remove("show-msg");
-                    }), 3e3);
-                }
-            }));
-        }
-        window.addEventListener("load", (function(e) {
-            const target = document.querySelector(".radio-buttons");
-            if (target) {
-                const config = {
-                    attributes: true,
-                    childList: true,
-                    subtree: true
-                };
-                function styleButtonChange() {
-                    const pickUpPointButtons = document.querySelectorAll(".radio-buttons__inner button, .radio-buttons__inner .btn");
-                    pickUpPointButtons.forEach((btn => {
-                        btn.setAttribute("class", "");
-                        btn.style = "display: flex; justify-content:center; align-items: center; text-align: center;";
-                        btn.classList.add("radio-buttons__btn", "btn", "btn_grey");
-                    }));
-                }
-                styleButtonChange();
-                const callback = function(mutationsList, observer) {
-                    for (let mutation of mutationsList) if (mutation.type === "childList") styleButtonChange();
-                };
-                const observer = new MutationObserver(callback);
-                observer.observe(target, config);
-            }
-        }));
-        const sidebarCatalogMenuChunk = document.querySelector(".sidebar-catalog__menu-chunk");
-        if (sidebarCatalogMenuChunk !== null) {
-            const sidebarCatalogMenu = sidebarCatalogMenuChunk.querySelector(".sidebar-catalog__menu");
-            const sidebarRect = sidebarCatalogMenuChunk.querySelector(".sidebar-catalog__hover-rect");
-            sidebarCatalogMenuChunk.addEventListener("mouseover", (e => {
-                let target = e.target.closest(".sidebar-catalog__link");
-                if (e.target.classList.contains("sidebar-catalog__link")) sidebarRect.style.bottom = `${sidebarCatalogMenu.offsetHeight - (target.offsetTop + target.clientHeight - sidebarCatalogMenu.scrollTop)}px`;
-            }));
-        }
-        function sidebarCatalogActions(e) {
-            const targetElement = e.target.closest("[data-parent]");
-            const activeLink = document.querySelector("._sub-menu-active");
-            const activeBlock = document.querySelector("._sub-menu-open");
-            if (targetElement) {
-                const subMenuId = targetElement.dataset.parent;
-                const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
-                if (subMenu) {
-                    if (activeLink !== targetElement) {
-                        if (activeLink) {
-                            activeLink.classList.remove("_sub-menu-active");
-                            activeBlock.classList.remove("_sub-menu-open");
-                            document.documentElement.classList.remove("sidebar-sub-catalog-open");
-                        }
-                        targetElement.classList.add("_sub-menu-active");
-                        subMenu.classList.add("_sub-menu-open");
-                        document.documentElement.classList.add("sidebar-sub-catalog-open");
-                    } else {
-                        activeLink.classList.remove("_sub-menu-active");
-                        activeBlock.classList.remove("_sub-menu-open");
-                        document.documentElement.classList.remove("sidebar-sub-catalog-open");
-                    }
-                    e.preventDefault();
-                }
-            } else if (activeLink) {
-                activeLink.classList.remove("_sub-menu-active");
-                activeBlock.classList.remove("_sub-menu-open");
-                document.documentElement.classList.remove("sidebar-sub-catalog-open");
-            }
-        }
-        if (document.querySelector(".broadcast")) {
-            let buttonToTop = function(e) {
-                let btnTop = document.querySelector(".broadcast");
-                let scr_val = window.pageYOffset + document.documentElement.clientHeight;
-                let scrollHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight);
-                scr_val >= scrollHeight - 50 ? btnTop.classList.add("_active") : btnTop.classList.remove("_active");
-            };
-            window.addEventListener("scroll", buttonToTop);
-        }
-        document.querySelectorAll(".float-line").forEach((e => {
-            floatLine(e);
-        }));
-        function floatLine(node) {
-            if (!node) return;
-            node.addEventListener("mouseover", (e => {
-                if (e.target.classList.contains("float-line__item")) if (node.closest(".float-line__horizontal")) {
-                    node.style.setProperty("--underline-offset-y", `${e.target.offsetTop}px`);
-                    node.style.setProperty("--underline-height", `${e.target.offsetHeight}px`);
-                } else {
-                    node.style.setProperty("--underline-width", `${e.target.offsetWidth}px`);
-                    node.style.setProperty("--underline-offset-x", `${e.target.offsetLeft}px`);
-                }
-            }));
-            node.addEventListener("mouseleave", (() => {
-                if (node.closest(".float-line__horizontal")) node.style.setProperty("--underline-height", "0"); else node.style.setProperty("--underline-width", "0");
-            }));
-        }
-        const layout = document.querySelector(".js-layout");
-        if (layout) {
-            if (localStorage.getItem("layout")) {
-                document.querySelector(".main-catalog__cards, .favorites__cards").classList.add("row");
-                layout.querySelector(".js-layout__row").classList.add("_active");
-                layout.querySelector(".js-layout__column").classList.remove("_active");
-            }
-            layout.addEventListener("click", (function(e) {
-                let target = e.target;
-                let cards = document.querySelector(".main-catalog__cards, .favorites__cards");
-                let rowBtn = layout.querySelector(".js-layout__row");
-                let colBtn = layout.querySelector(".js-layout__column");
-                if (target.closest(".js-layout__column")) {
-                    cards.classList.remove("row");
-                    localStorage.removeItem("layout");
-                } else if (target.closest(".js-layout__row")) {
-                    cards.classList.add("row");
-                    localStorage.setItem("layout", "row");
-                }
-                if (target.closest(".js-layout__row")) {
-                    colBtn.classList.remove("_active");
-                    rowBtn.classList.add("_active");
-                } else {
-                    colBtn.classList.add("_active");
-                    rowBtn.classList.remove("_active");
-                }
-            }));
-        }
-        window.addEventListener("load", (function() {
-            const dataName = Array.from(document.querySelectorAll("[data-name]"));
-            let names = [];
-            dataName.forEach((el => {
-                if (!names.includes(el.dataset.name)) names.push(el.dataset.name);
-            }));
-            for (const name of names) setHeight(name);
-            function setHeight(name) {
-                const nodeName = document.querySelector(`[data-main=${name}]`);
-                const node = document.querySelectorAll(`[data-name=${name}]`);
-                let heights = [];
-                heights.push(nodeName.scrollHeight);
-                node.forEach((el => {
-                    heights.push(el.scrollHeight);
-                }));
-                let maxHei = Math.max(...heights);
-                node.forEach((element => {
-                    element.style.height = maxHei + "px";
-                }));
-                nodeName ? nodeName.style.height = maxHei + "px" : null;
-            }
-            let btnChek = document.querySelector('.radio-inline input[type="radio"]:checked');
-            if (btnChek) btnChek.closest(".radio-inline").classList.add("checked");
-        }));
-        document.addEventListener("change", (e => {
-            let target = e.target;
-            if (target.classList.contains("js-allCheck")) {
-                let table = target.closest(".merchant-cabinet__table");
-                let checkboxes = table.querySelectorAll(".merchant-cabinet__checkbox input");
-                target.checked ? checkboxes.forEach((e => e.checked = true)) : checkboxes.forEach((e => e.checked = false));
-            }
-        }));
         document.addEventListener("click", (function(e) {
             const target = e.target;
             const ddWrapper = target.closest("[data-dropdown]");
@@ -11050,6 +10858,26 @@ PERFORMANCE OF THIS SOFTWARE.
                 e.preventDefault();
             }
         }
+        const handleMenuInteraction = (e, mediaQuery, targetItem, activeClass) => {
+            if (window.matchMedia(mediaQuery).matches) {
+                const openMenu = document.querySelector(`.${activeClass}`);
+                const targetElement = e.target.closest(targetItem);
+                if (openMenu && openMenu !== targetElement) openMenu.classList.remove(activeClass);
+                if (targetElement) {
+                    targetElement.classList.add(activeClass);
+                    if (targetElement === openMenu && e.type === "click") openMenu.classList.remove(activeClass);
+                }
+            }
+        };
+        document.addEventListener("click", (e => {
+            const isSubMenu = e.target.closest("[data-submenu-drop]");
+            if (isSubMenu) e.stopPropagation(); else handleMenuInteraction(e, "(max-width:1199.98px)", "[data-menu-drop]", "menu__item_open");
+            handleMenuInteraction(e, "(max-width:1199.98px)", "[data-submenu-drop]", "_sub-menu-open");
+        }));
+        document.addEventListener("mouseover", (e => {
+            handleMenuInteraction(e, "(min-width:1199.98px)", "[data-menu-drop]", "menu__item_open");
+            handleMenuInteraction(e, "(min-width:1199.98px)", "[data-submenu-drop]", "_sub-menu-open");
+        }));
         window["FLS"] = true;
         isWebp();
         addLoadedClass();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,28 +6,6 @@ import { flsModules } from "./modules.js";
 //#region Глобальный клик
 
 document.addEventListener("click", function (e) {
-   sidebarCatalogActions(e)
-   // открыть модалку каталога
-   // if (bodyLockStatus && e.target.closest('.js-open-sidebar-catalog')) {
-   //    bodyLockToggle();
-   //    document.documentElement.classList.toggle("sidebar-catalog-open");
-   //    if (window.matchMedia("(min-width: 991.98px)").matches && !isMobile.any()) {
-   //       document.addEventListener("mouseover", sidebarCatalogActions);
-   //       document.removeEventListener("click", sidebarCatalogActions);
-   //    } else {
-   //       document.addEventListener("click", sidebarCatalogActions);
-   //       document.removeEventListener("mouseover", sidebarCatalogActions);
-   //    }
-   // }
-   // закрыть модалку каталога
-   // if (e.target.closest('.js-sidebar-catalog-close')) {
-   //    bodyLockToggle();
-   //    document.documentElement.classList.remove("sidebar-catalog-open", "sidebar-sub-catalog-open");
-   // }
-   // if (!e.target.closest('.sidebar-catalog') && document.querySelector('.sidebar-catalog-open') && !e.target.closest('.js-open-sidebar-catalog')) {
-   //    bodyLockToggle();
-   //    document.documentElement.classList.remove("sidebar-catalog-open", "sidebar-sub-catalog-open");
-   // }
    // очистка input по клику на крестик
    if (e.target.closest('.form__clear-svg')) {
       let input = e.target.closest('.form__line').querySelector('.form__input') || e.target.closest('.form__line').querySelector('.form__txt');
@@ -42,36 +20,7 @@ document.addEventListener("click", function (e) {
    if (e.target.closest('textarea')) {
       txtarAutoHeight(e.target)
    }
-   // спрятать/показать input в личкабе
-   if (e.target.closest('.personal-data__change')) {
-      changeData(e.target)
-      e.preventDefault()
-   }
-   // смена текста кнопки в личкабе
-   if (e.target.closest('.order__more-btn')) {
-      let target = e.target.closest('.order__more-btn')
-      target.classList.contains('_spoller-active') ? target.innerHTML = 'Свернуть детали заказа' : target.innerHTML = 'Показать детали заказа';
-      e.preventDefault()
-   }
 });
-
-//#endregion
-
-//#region Перемещение модалки с фильтрами под .wrapper
-
-const filtersPopup = document.querySelector('#filters-more');
-
-if (filtersPopup) {
-   filtersPopup.remove();
-   document.querySelector('.popup-box').insertAdjacentElement("beforeend", filtersPopup);
-   getFilterColumns(filtersPopup);
-}
-
-function getFilterColumns(popup) {
-   const columns = popup.querySelectorAll('.filters__col');
-   const popupWrapper = popup.querySelector('.filters__wrapper');
-   columns.length > 1 ? popupWrapper.classList.add('many-cols') : null;
-}
 
 //#endregion
 
@@ -153,278 +102,6 @@ function txtarAutoHeight(target) {
 
 //#endregion
 
-//#region спрятать/показать input в личкабе
-
-function changeData(target) {
-   let el = target.closest('.personal-data__row')
-   el.classList.add('_active');
-   let submitBtn = el.querySelector('.personal-data__btn')
-   submitBtn.addEventListener("click", function (e) {
-      el.classList.remove('_active');
-      el.classList.add('show-msg');
-      setTimeout(() => {
-         el.classList.remove('show-msg');
-      }, 3000);
-   });
-   document.addEventListener('keydown', function (e) {
-      if (e.code === 'Escape' || e.code === 'Enter' || e.code === 'NumpadEnter') {
-         el.classList.remove('_active');
-         el.classList.add('show-msg');
-         setTimeout(() => {
-            el.classList.remove('show-msg')
-         }, 3000);
-      }
-   });
-}
-
-//#endregion
-
-//#region Добавление классов для кнопок на странице оформления при загрузке и обновлении сстраницы
-
-window.addEventListener("load", function (e) {
-   const target = document.querySelector('.radio-buttons');
-   if (target) {
-
-      const config = {
-         attributes: true,
-         childList: true,
-         subtree: true
-      };
-
-      function styleButtonChange() {
-         const pickUpPointButtons = document.querySelectorAll('.radio-buttons__inner button, .radio-buttons__inner .btn');
-
-         pickUpPointButtons.forEach(btn => {
-            btn.setAttribute('class', '')
-            btn.style = 'display: flex; justify-content:center; align-items: center; text-align: center;';
-            btn.classList.add('radio-buttons__btn', 'btn', 'btn_grey');
-         });
-      }
-      styleButtonChange();
-
-      const callback = function (mutationsList, observer) {
-         for (let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-               styleButtonChange();
-            }
-         }
-      };
-
-      const observer = new MutationObserver(callback);
-
-      observer.observe(target, config);
-   }
-});
-
-//#endregion
-
-//#region hover на ссылках в боковом каталоге
-
-const sidebarCatalogMenuChunk = document.querySelector('.sidebar-catalog__menu-chunk');
-if (sidebarCatalogMenuChunk !== null) {
-   const sidebarCatalogMenu = sidebarCatalogMenuChunk.querySelector('.sidebar-catalog__menu');
-   const sidebarRect = sidebarCatalogMenuChunk.querySelector('.sidebar-catalog__hover-rect')
-   sidebarCatalogMenuChunk.addEventListener('mouseover', (e) => {
-      let target = e.target.closest('.sidebar-catalog__link');
-      if (e.target.classList.contains('sidebar-catalog__link')) {
-         sidebarRect.style.bottom = `${sidebarCatalogMenu.offsetHeight - ((target.offsetTop + target.clientHeight) - sidebarCatalogMenu.scrollTop)}px`
-      }
-   })
-}
-
-//#endregion
-
-//#region Открыть/закрыть боковой каталог + Открытие закрытие подкатегорий в каталоге
-
-function sidebarCatalogActions(e) {
-   const targetElement = e.target.closest('[data-parent]');
-   const activeLink = document.querySelector('._sub-menu-active');
-   const activeBlock = document.querySelector('._sub-menu-open');
-
-   if (targetElement) {
-      const subMenuId = targetElement.dataset.parent;
-      const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
-
-      if (subMenu) {
-         if (activeLink !== targetElement) {
-            if (activeLink) {
-               activeLink.classList.remove('_sub-menu-active');
-               activeBlock.classList.remove('_sub-menu-open');
-               document.documentElement.classList.remove('sidebar-sub-catalog-open');
-            }
-
-            targetElement.classList.add('_sub-menu-active');
-            subMenu.classList.add('_sub-menu-open');
-            document.documentElement.classList.add('sidebar-sub-catalog-open');
-         } else {
-            activeLink.classList.remove('_sub-menu-active');
-            activeBlock.classList.remove('_sub-menu-open');
-            document.documentElement.classList.remove('sidebar-sub-catalog-open');
-         }
-         e.preventDefault();
-      }
-   } else {
-      if (activeLink) {
-         activeLink.classList.remove('_sub-menu-active');
-         activeBlock.classList.remove('_sub-menu-open');
-         document.documentElement.classList.remove('sidebar-sub-catalog-open');
-      }
-   }
-}
-//#endregion
-
-//#region Кнопка вверх и лого
-
-if (document.querySelector('.broadcast')) {
-
-   let buttonToTop = function (e) {
-      let btnTop = document.querySelector('.broadcast');
-      let scr_val = window.pageYOffset + document.documentElement.clientHeight;
-      let scrollHeight = Math.max(
-         document.body.scrollHeight, document.documentElement.scrollHeight,
-         document.body.offsetHeight, document.documentElement.offsetHeight,
-         document.body.clientHeight, document.documentElement.clientHeight
-      );
-      scr_val >= (scrollHeight - 50) ? btnTop.classList.add('_active') : btnTop.classList.remove('_active');
-   };
-   window.addEventListener('scroll', buttonToTop);
-}
-//#endregion
-
-//#region Плавающая линия для табов
-
-document.querySelectorAll(".float-line").forEach(e => {
-   floatLine(e)
-});
-
-function floatLine(node) {
-   if (!node) return
-
-   node.addEventListener("mouseover", (e) => {
-      if (e.target.classList.contains("float-line__item")) {
-         if (node.closest('.float-line__horizontal')) {
-            node.style.setProperty(
-               "--underline-offset-y",
-               `${e.target.offsetTop}px`
-            );
-            node.style.setProperty(
-               "--underline-height",
-               `${e.target.offsetHeight}px`
-            );
-         } else {
-            node.style.setProperty(
-               "--underline-width",
-               `${e.target.offsetWidth}px`
-            );
-            node.style.setProperty(
-               "--underline-offset-x",
-               `${e.target.offsetLeft}px`
-            );
-         }
-      }
-   });
-   node.addEventListener("mouseleave", () => {
-      if (node.closest('.float-line__horizontal')) {
-         node.style.setProperty("--underline-height", "0")
-      } else {
-         node.style.setProperty("--underline-width", "0")
-      }
-   });
-}
-
-//#endregion
-
-//#region Переключатель отображения плиток в каталоге
-
-const layout = document.querySelector('.js-layout');
-if (layout) {
-
-
-
-   if (localStorage.getItem('layout')) {
-      document.querySelector('.main-catalog__cards, .favorites__cards').classList.add('row');
-      layout.querySelector(".js-layout__row").classList.add('_active');
-      layout.querySelector(".js-layout__column").classList.remove('_active');
-   }
-
-
-   layout.addEventListener("click", function (e) {
-      let target = e.target;
-      let cards = document.querySelector('.main-catalog__cards, .favorites__cards')
-      let rowBtn = layout.querySelector(".js-layout__row")
-      let colBtn = layout.querySelector(".js-layout__column")
-
-
-
-      if (target.closest('.js-layout__column')) {
-         cards.classList.remove('row')
-         localStorage.removeItem('layout')
-      } else if (target.closest('.js-layout__row')) {
-         cards.classList.add('row')
-         localStorage.setItem("layout", 'row')
-      }
-      if (target.closest('.js-layout__row')) {
-         colBtn.classList.remove('_active')
-         rowBtn.classList.add('_active')
-      } else {
-         colBtn.classList.add('_active')
-         rowBtn.classList.remove('_active')
-      }
-   });
-}
-
-//#endregion
-
-//#region высота строк в сравнении 
-
-window.addEventListener("load", function () {
-   const dataName = Array.from(document.querySelectorAll('[data-name]'));
-   let names = [];
-   dataName.forEach(el => {
-      if (!names.includes(el.dataset.name)) {
-         names.push(el.dataset.name)
-      }
-   });
-   for (const name of names) {
-      setHeight(name)
-   }
-   function setHeight(name) {
-      const nodeName = document.querySelector(`[data-main=${name}]`);
-      const node = document.querySelectorAll(`[data-name=${name}]`);
-      let heights = []
-      heights.push(nodeName.scrollHeight);
-      node.forEach(el => {
-         heights.push(el.scrollHeight);
-      });
-      let maxHei = Math.max(...heights);
-      node.forEach(element => {
-         element.style.height = maxHei + 'px';
-      });
-      nodeName ? nodeName.style.height = maxHei + 'px' : null;
-   }
-   let btnChek = document.querySelector(".radio-inline input[type=\"radio\"]:checked");
-   if (btnChek) {
-      btnChek.closest('.radio-inline').classList.add('checked');
-   }
-});
-
-//#endregion
-
-//#region выбор всех чекбоксов
-
-// js-allCheck
-
-document.addEventListener('change', e => {
-   let target = e.target;
-   if (target.classList.contains('js-allCheck')) {
-      let table = target.closest('.merchant-cabinet__table')
-      let checkboxes = table.querySelectorAll('.merchant-cabinet__checkbox input');
-      target.checked ? checkboxes.forEach(e => e.checked = true) : checkboxes.forEach(e => e.checked = false);
-   }
-});
-
-//#endregion
-
 //#region Функционал дропдаунов открыть\закрыть
 
 document.addEventListener("click", function (e) {
@@ -452,5 +129,44 @@ function dropdownAction(e, ddWrapper, ddActive) {
       e.preventDefault();
    }
 }
+
+//#endregion
+
+//#region Функционал выпадашек в меню
+
+const handleMenuInteraction = (e, mediaQuery, targetItem, activeClass) => {
+   if (window.matchMedia(mediaQuery).matches) {
+      const openMenu = document.querySelector(`.${activeClass}`);
+      const targetElement = e.target.closest(targetItem);
+
+      if (openMenu && openMenu !== targetElement) {
+         openMenu.classList.remove(activeClass);
+      }
+
+      if (targetElement) {
+         targetElement.classList.add(activeClass);
+         if (targetElement === openMenu && e.type === 'click') {
+            openMenu.classList.remove(activeClass);
+         }
+      }
+   }
+};
+
+document.addEventListener('click', (e) => {
+   const isSubMenu = e.target.closest('[data-submenu-drop]');
+
+   if (isSubMenu) {
+      e.stopPropagation(); // предотвращаем всплытие события, чтобы основное меню не закрывалось
+   } else {
+      handleMenuInteraction(e, '(max-width:1199.98px)', '[data-menu-drop]', 'menu__item_open');
+   }
+
+   handleMenuInteraction(e, '(max-width:1199.98px)', '[data-submenu-drop]', '_sub-menu-open');
+});
+
+document.addEventListener('mouseover', (e) => {
+   handleMenuInteraction(e, '(min-width:1199.98px)', '[data-menu-drop]', 'menu__item_open');
+   handleMenuInteraction(e, '(min-width:1199.98px)', '[data-submenu-drop]', '_sub-menu-open');
+});
 
 //#endregion
